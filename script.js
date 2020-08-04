02d8c41917dda53df1cf492713400d805e5204bd
Dando513 <49767042+Dando513@users.noreply.github.com>
2020-08-04 16:02:21 -0400

    fixing local storage

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
 var timeArray = [9,10,11,12,13,14,15,16,17] 
+var time = moment().hours()
+
 
 
 $.each(timeArray, function(index, value){
  var inputGroup = $('<div class="input-group mb-3"></div>')
+ var correctedHour = timeArray[index];
+if(correctedHour >12){
+correctedHour-=12;
+}
+var inputSpan = $('<span class="input-group-text">'+correctedHour+':00<span>')
  var inputPrepend = $('<div class="input-group-prepend"></div>')
- var inputSpan = $('<span class="input-group-text">'+timeArray[index]+':00</span>')
-
-function timeAdjust(){
-    if(input>12, input -=12)
-
- var ppf ="";
- var time = moment().hours
  
- console.log(time)
- if(input>time){
-  ppf="future"   
+var ppf ="";
+
+function timeDay(input){
+    if(input>time){
+  ppf="future"
  }
 else if (input==time){
  ppf="present"
 }
  else if (input<time){
-    ppf="future"
+    ppf="past"
  }
- console.log(ppf)
 }
+timeDay(value);
 
- 
+
  inputPrepend.append(inputSpan)
- var input = $('<input type="text" class="form-control"+ppf>')
- var button = $('<button>Save</button>')
- 
- inputGroup.append(inputPrepend,input,button)
- 
- $("#timeDiv").append(inputGroup);
-})
+ var input = $('<input type="text">');
+input.addClass("form-control");
+input.addClass(ppf);
+ var button = $('<button>Save</button>');
+inputGroup.append(inputPrepend,input,button);
+
+$("#timeDiv").append(inputGroup);
+});
 $("button").on("click",function(){
 
-var textInput=$(this).siblings("input").val()
-console.log(textInput)
-})
-
+var textInput=$(this).siblings("input").val();
+console.log(textInput);  
+});
 
 
 
