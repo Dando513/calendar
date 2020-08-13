5ee96a728a843df9bfb573c784245631cf2fb660
Dando513 <49767042+Dando513@users.noreply.github.com>
2020-08-13 17:25:27 -0400

    final version

--- script.js
+++ script.js
@@ -1,51 +1,57 @@
-var timeArray = [9,10,11,12,13,14,15,16,17] 
+var timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 var time = moment().hours()
 
 
+$.each(timeArray, function (index, value) {
+   var correctedHour = timeArray[index];
+   if (correctedHour > 12) {
+      correctedHour -= 12;
+   }
+   
+   var ppf = "";
 
-$.each(timeArray, function(index, value){
- var inputGroup = $('<div class="input-group mb-3"></div>')
- var correctedHour = timeArray[index];
-if(correctedHour >12){
-correctedHour-=12;
-}
-var inputSpan = $('<span class="input-group-text">'+correctedHour+':00<span>')
- var inputPrepend = $('<div class="input-group-prepend"></div>')
- 
-var ppf ="";
+   function timeDay(input) {
+      if (input > time) {
+         ppf = "future"
+      }
+      else if (input == time) {
+         ppf = "present"
+      }
+      else if (input < time) {
+         ppf = "past"
+      }
+   }
 
-function timeDay(input){
-    if(input>time){
-  ppf="future"
- }
-else if (input==time){
- ppf="present"
-}
- else if (input<time){
-    ppf="past"
- }
-}
-timeDay(value);
+   timeDay(value);
 
+   var inputGroup = $('<div class="input-group mb-3"></div>')
+   
+   var inputSpan = $('<span class="input-group-text">' + correctedHour + ':00<span>')
+   var inputPrepend = $('<div class="input-group-prepend"></div>')
+   inputPrepend.append(inputSpan)
+   
+   var input = $('<input type="text" id="'+timeArray[index]+'">');
+   input.addClass("form-control");
+   input.addClass(ppf);
+   var savedText = localStorage.getItem(timeArray[index])
+   console.log(savedText)
+   input.val(savedText)
 
- inputPrepend.append(inputSpan)
- var input = $('<input type="text">');
-input.addClass("form-control");
-input.addClass(ppf);
- var button = $('<button>Save</button>');
-inputGroup.append(inputPrepend,input,button);
+   var button = $('<button>Save</button>');
+   
+   inputGroup.append(inputPrepend, input, button);
 
-$("#timeDiv").append(inputGroup);
+   $("#timeDiv").append(inputGroup);
 });
-$("button").on("click",function(){
 
-var textInput=$(this).siblings("input").val();
-console.log(textInput);  
+$("button").on("click", function () {
 
-localStorage.setItem("9",textInput);
-});
+   var textInput = $(this).siblings("input").val();
+   console.log(textInput);
+   var hr = $(this).siblings("input").attr("id")
+   console.log(hr);
 
+   localStorage.setItem(hr,textInput);
 
-
-
+})
 
